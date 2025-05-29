c8f0b389b71b1afdf3d682df4a153fd536c9587a
Ubuntu <ubuntu@ip-172-31-11-195.ap-south-1.compute.internal>
2025-05-29 18:04:51 +0000

    Initial commit with Medusa and Terraform setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,24 +4,31 @@ provider "aws" {
 
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "medusa-vpc"
+  }
 }
 
-# Example Subnet (you likely have more in your version)
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "medusa-public-subnet"
+  }
 }
 
 resource "aws_security_group" "ecs_sg" {
   name        = "ecs-security-group"
-  description = "Allow HTTP inbound traffic"
+  description = "Allow traffic to ECS task"
   vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 9000
+    to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -31,6 +38,10 @@ resource "aws_security_group" "ecs_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-sg"
   }
 }
 
@@ -68,9 +79,8 @@ resource "aws_ecs_task_definition" "medusa_task" {
   memory                  = "512"
   execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode(
-    jsondecode(file("${path.module}/ecs-task-def.json"))
-  )
+  # No need to decode and re-encode the JSON file
+  container_definitions = file("${path.module}/ecs-task-def.json")
 }
 
 resource "aws_ecs_service" "medusa_service" {
