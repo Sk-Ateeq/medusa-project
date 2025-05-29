ef9b97b7823258a8bc195603e8b9d09fc9024c2a
Ubuntu <ubuntu@ip-172-31-11-195.ap-south-1.compute.internal>
2025-05-29 16:59:35 +0000

    Add Medusa ECS deployment with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,54 +1,25 @@
-# main.tf
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "medusa-vpc"
-  }
 }
 
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-1"
-  }
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public_assoc_1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_security_group" "medusa_sg" {
-  name        = "medusa-sg"
-  description = "Allow HTTP"
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs_sg"
+  description = "Allow all traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -58,18 +29,30 @@ resource "aws_security_group" "medusa_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "medusa-sg"
-  }
 }
 
 resource "aws_ecs_cluster" "medusa_cluster" {
   name = "medusa-cluster"
 }
 
-resource "aws_ecr_repository" "medusa_ecr" {
-  name = "medusa-backend"
+resource "aws_iam_role" "ecs_task_exec_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_policy" {
+  role       = aws_iam_role.ecs_task_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_task_definition" "medusa_task" {
@@ -78,43 +61,8 @@ resource "aws_ecs_task_definition" "medusa_task" {
   network_mode            = "awsvpc"
   cpu                     = "256"
   memory                  = "512"
-  execution_role_arn      = aws_iam_role.ecsTaskExecutionRole.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "medusa"
-      image     = "${aws_ecr_repository.medusa_ecr.repository_url}:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 9000
-          hostPort      = 9000
-        }
-      ]
-    }
-  ])
-}
-
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  execution_role_arn      = aws_iam_role.ecs_task_exec_role.arn
+  container_definitions   = file("${path.module}/ecs-task-def.json")
 }
 
 resource "aws_ecs_service" "medusa_service" {
@@ -125,13 +73,9 @@ resource "aws_ecs_service" "medusa_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [aws_subnet.public_subnet_1.id]
-    security_groups = [aws_security_group.medusa_sg.id]
+    subnets          = [aws_subnet.subnet.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.ecs_task_execution_role_policy
-  ]
 }
 
